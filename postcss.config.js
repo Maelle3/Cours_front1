9b1ea0b790b8217b644c73bb06e72c956c7a1299
Jeremy <jeremy.fardilha@centrale-marseille.fr>
2019-10-10 15:15:03 +0200

    postcss stuff

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,12 @@
+
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
-
     plugins: [
-
+        require('postcss-import'),
         require('tailwindcss'),
-
+        purgecss({
+            content: ['./*.html']
+        }),
     ]
-
 };
